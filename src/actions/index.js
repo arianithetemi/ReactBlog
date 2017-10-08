43a2bfa8a9723c178f72f474351194ac115be32b
arianithetemi <nitihetemi@gmail.com>
2017-10-08 22:32:29 +0200

    Action Creator hooked to component, showing posts from api into DOM

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=J0071';
 
 export function fetchPosts() {
